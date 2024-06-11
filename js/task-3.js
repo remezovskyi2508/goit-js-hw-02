374e5544da43ded6e792772b9bebf0b2a1336c89
Anton Remezovskyi <antonremezovskyi@gmail.com>
2024-06-11 11:46:10 +0300

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+function checkForSpam(message) {
+    const formatSms = message.toLowerCase();
+    return (formatSms.includes("spam") || formatSms.includes("sale"));
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
